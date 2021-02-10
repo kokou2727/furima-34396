559ce357f0946cf188e2577c5b36751435f767b4
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-10 20:50:21 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX, message: "Include both letters and numbers" }
-  
+
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "Full-width characters" }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "Full-width characters" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "Full-width characters" }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "Full-width characters" }
     validates :first_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
     validates :last_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" }
     validates :birthday
