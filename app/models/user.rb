813f14af8ccd1c4d8642d3dc72de26ac5dd70bb1
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-10 14:59:44 +0900

    バリデーションの修正とnicknameのDBへの保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, with: PASSWORD_REGEX 
+  # PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+
+  validates :password, format: { with: PASSWORD_REGEX, message: "test" }
   validates :nickname, presence: true
-  validates :first_name, presence: true, with: /\A[ぁ-んァ-ン一-龥]/
-  validates :last_name, presence: true, with: /\A[ぁ-んァ-ン一-龥]/
-  validates :first_furigana, presence: true, with: /\A[ァ-ヶー－]+\z/
-  validates :last_furigana, presence: true, with: /\A[ァ-ヶー－]+\z/
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "test" }
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "test" }
+  validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "test" }
+  validates :last_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "test" }
   validates :birthday, presence: true
 end
