73774a7683efd4e287506ce9a3128231917d7927
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-11 15:50:54 +0900

    ActiveHashを用いたプルダウンの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title,:description,:category_id,:state_id,:fee_id,:from_id,:shipping_date_id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,20 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :description
-    validates :category_id
-    validates :state_id
     validates :price, format: { with: /\A[0-9]+\z/, message: "Price Half-width number" }, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 , message: "Price Out of setting range"}
-    validates :from_id
-    validates :shipping_date_id
-    validates :fee_id
   end
 
+  validates :category_id, numericality: { other_than: 1 }
+  validates :fee_id, numericality: { other_than: 1 }
+  validates :shipping_date_id, numericality: { other_than: 1 }
+  validates :from_id, numericality: { other_than: 1 }
+  validates :state_id, numericality: { other_than: 1 }
+
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :fee
+  belongs_to :ShippingDate
+  belongs_to :from
+  belongs_to :state
 end
