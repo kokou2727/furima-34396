7867f3b372b9c701a66882aa4ea9d77e6f310295
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-11 23:34:43 +0900

    商品の一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
-    # @items = Item.order('created_at DESC')
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -21,7 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :price, :category_id, :state_id, :fee_id, :from_id, :shipping_date_id,
-                                 :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :description, :price, :category_id, :state_id, :fee_id, :from_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 end
