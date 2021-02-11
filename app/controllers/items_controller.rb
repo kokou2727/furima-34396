5bdf47289d4fa129fde50652b73b5636bdee8f05
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-11 17:13:01 +0900

    販売手数料や販売利益の計算

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit( :title, :description, :category_id, :state_id, :fee_id, :from_id, :shipping_date_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit( :title, :description, :price, :category_id, :state_id, :fee_id, :from_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :description
+    validates :image
     validates :price, format: { with: /\A[0-9]+\z/, message: "Price Half-width number" }, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 , message: "Price Out of setting range"}
   end
 
