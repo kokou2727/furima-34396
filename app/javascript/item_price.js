4358c110a1320d11f916ffd34bd68671226ce7ad
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-11 17:18:41 +0900

    コメントアウトの削除など

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,6 @@
 function price( ){
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", (e) => {
+  priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
