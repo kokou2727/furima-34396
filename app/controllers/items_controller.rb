e03522a51739e6cd95a0905b46b96247323fd77d
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-12 17:08:15 +0900

    商品編集ページへの遷移の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
-
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
   def index
     @items = Item.order('created_at DESC')
   end
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
       redirect_to item_path(item.id)
     else
       render :edit
+    end
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id 
+      redirect_to action: :index
     end
   end
 
