44dccaf34105554212a6bde5ca2aa7c2499c64bc
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-12 17:55:48 +0900

    コントローラーの記述の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: :edit
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
+  before_action :move_to_index, only: [:edit, :update]
+  
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -35,9 +34,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  def move_to_index
+  def move_to_index  
+    redirect_to action: :index unless current_user.id == @item.user_id
+  end
+
+  def set_item
     @item = Item.find(params[:id])
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 
   private
