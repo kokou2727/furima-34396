d7911544d8ba9340f0cbc500e73b7987cce6e782
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-13 22:25:24 +0900

    バリデーションの修正

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -9,9 +9,8 @@ class RecordAddress
     validates :user_id
     validates :item_id
     validates :record_id
-    validates :phone_number numericality: {with: /\A\d{7}\z/, message: "Input only number"}
+    validates :phone_number numericality: {with: /\A\d{11}\z/, message: "Input only number"}
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "Select"}
-  
 end
