66a1360300c09a3ead0747027be95359220c31b9
kokou2727 <kojinakasuji523025@gmail.com>
2021-02-15 16:10:16 +0900

    building_nameのバリデーションの撤回とクレカ以外の購入機能の実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,27 @@
 class RecordsController < ApplicationController
+  before_action :set_item, only:[:index, :create]
+
   def index
+    @record_address = RecordAddress.new
   end
 
   def create
+    @record_address = RecordAddress.new(record_params)
+    if @record_address.valid?
+      @record_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  private
+
+  def record_params
+    params.require(:record_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -5,12 +5,15 @@ class RecordAddress
   with_options presence: true do
     validates :city
     validates :address
-    validates :building_name
     validates :user_id
     validates :item_id
-    validates :record_id
-    validates :phone_number numericality: {with: /\A\d{11}\z/, message: "Input only number"}
+    validates :phone_number, numericality: {with: /\A\d{11}\z/, message: "Input only number"}
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "Select"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "Select"}
+
+  def save
+    record = Record.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, phone_number: phone_number, building_name: building_name, address: address, record_id: record.id)
+  end
 end
